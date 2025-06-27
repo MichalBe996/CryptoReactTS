861757b5b52237f1b98ec988e1d38d77ce07624f
Michał <michal.bielawski@hotmail.com>
2025-06-27 11:19:14 +0200

    created about page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
 import {Routes, Route} from "react-router-dom"
 import "react-bootstrap"
+
 
 
 function App() {
@@ -11,7 +14,8 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/dashboard" element={<h1>Test dashboards</h1>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/about" element={<About/>}/>
     </Routes>
     </>
     
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
 
 const Navbar = () => {
+    const navigate = useNavigate()
+    function navigateHome() {
+        navigate("/")
+    }
+    function navigateDash(){
+        navigate("/dashboard")
+    }
+
   return (
     <nav className='navbar'>
         <div className='logo-div'>
             <h1>LOGO placeholder</h1>
         </div>
         <div className="nav-buttons">
-            <button>Home</button>
-            <button>Dashboard</button>
+            <button onClick={navigateHome}>Home</button>
+            <button onClick={navigateDash}>Dashboard</button>
             <button>About</button>
         </div>
 
