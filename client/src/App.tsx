8a502d0f5dc1fa61df01673801cf690a67a5cc08
Michał <michal.bielawski@hotmail.com>
2025-06-27 11:27:24 +0200

    setting up bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,21 @@ import './App.css';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
-import {Routes, Route} from "react-router-dom"
-import "react-bootstrap"
+import {Routes, Route, Navigate} from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 
 
 function App() {
   return (
-    <>
+    <Container className="my-4">
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="*" element={<Navigate to="/"/>}/>
     </Routes>
-    </>
+    </Container>
     
   );
 }
