0219166f4bbfbcf764a891902b99150ada87d995
Michał <michal.bielawski@hotmail.com>
2025-06-27 11:14:33 +0200

    created navbar component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='logo-div'>
+            <h1>LOGO placeholder</h1>
+        </div>
+        <div className="nav-buttons">
+            <button>Home</button>
+            <button>Dashboard</button>
+            <button>About</button>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
