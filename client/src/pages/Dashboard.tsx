aa86891e532b589af32d2535a0798ac91a718885
Michał <michal.bielawski@hotmail.com>
2025-08-26 15:04:29 +0200

    implementing crypto dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
+
+
+
+
 
 const Dashboard = () => {
-  return (
+  const [pageLoading, setPageLoading] = React.useState<boolean>(true)
+  const apiUrl = "http://localhost:8080/api/v1/data"
+  const [apiData, setApiData] = React.useState([])
+  const getApiData = (url: string) => {
+
+  axios.get(url, {
+    headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+
+  } )
+  .then(res => setApiData(res.data.data))
+  .catch(err=> console.log(err))
+  setPageLoading(false)
+
+}
+
+React.useEffect(()=> {
+  getApiData(apiUrl)
+}, [])
+  return ( pageLoading? <h1>Loading...</h1> :
     <div>
         <Navbar/>
     </div>
