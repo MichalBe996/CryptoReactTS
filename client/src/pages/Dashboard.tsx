2643574629a0df5a323f09cc9d21c9f8b836c8a5
Michał <michal.bielawski@hotmail.com>
2025-08-29 14:19:12 +0200

    styling the chart

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -11,6 +11,7 @@ const Dashboard = () => {
   const [pageLoading, setPageLoading] = React.useState<boolean>(true)
   const apiUrl = "http://localhost:8080/api/v1/data"
   const [apiData, setApiData] = React.useState([])
+  const [cryptoNames, setCryptoNames] = React.useState<string[]>([])
   const getApiData = (url: string) => {
 
   axios.get(url, {
@@ -39,11 +40,12 @@ React.useEffect(()=> {
   return ( pageLoading? <h1>Loading...</h1> :
     <div>
         <Navbar/>
-        <BarChart className='chart' 
+        <BarChart 
         xAxis={[
           {
           id: "cryptoNames",
           data: ["test1", "test2", "test3"],
+          
           
           
         }
@@ -60,8 +62,39 @@ React.useEffect(()=> {
           },
           
         ]}
-        height={400}
+        
+        height={300}
+        width={500}
         borderRadius={10}
+        sx={{
+    //change left yAxis label styles
+   "& .MuiChartsAxis-left .MuiChartsAxis-tickLabel":{
+    strokeWidth:"0.4",
+    fill:"white",
+    stroke: "white"
+   },
+   // change all labels fontFamily shown on both xAxis and yAxis
+   "& .MuiChartsAxis-tickContainer .MuiChartsAxis-tickLabel":{
+       fontFamily: "Roboto",
+    },
+    // change bottom label styles
+    "& .MuiChartsAxis-bottom .MuiChartsAxis-tickLabel":{
+        strokeWidth:"0.5",
+        fill: "white"
+     },
+      // bottomAxis Line Styles
+     "& .MuiChartsAxis-bottom .MuiChartsAxis-line":{
+      stroke: "white",
+      strokeWidth:0.4
+     },
+     // leftAxis Line Styles
+     "& .MuiChartsAxis-left .MuiChartsAxis-line":{
+      stroke:"white",
+      fill: "white",
+      strokeWidth:0.4
+     }
+  }}
+        
         />
         
     </div>
